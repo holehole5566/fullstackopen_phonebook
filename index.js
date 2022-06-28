e5ec0ed8702775f3846e152eafbaca6a462aec20
holehole5566 <jd965303@gmail.com>
2022-06-28 21:49:52 +0800

    heroku

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
-let phonebook = [
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+let persons = [
     {
       "id": 1,
       "name": "Arto Hellas",
@@ -24,15 +38,57 @@ let phonebook = [
     }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/api/phonebook', (request, response) => {
-  response.json(phonebook)
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
 })
 
-const PORT = 3001
+app.get('/info', (request, response) => {
+  const time =new Date().getTime()
+  const datetime = new Date(time)
+  response.end(`Phonebook has people for ${persons.length} people\n${datetime}`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+
+  const body = request.body
+
+  if (!body.name||!body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const person = persons.find(person => person.name === body.name)
+  if (person) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
+})
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
